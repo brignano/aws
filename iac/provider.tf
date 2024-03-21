321bde130baa33c6ad89e0c33f1441583fd9df2a
anthony <abrignano@comcast.net>
2024-03-21 22:16:22 +0000

    add `default_tags` for `aws` provider

--- iac/provider.tf
+++ iac/provider.tf
@@ -14,4 +14,13 @@ terraform {
 
 provider "aws" {
   region = local.region
+  default_tags {
+    tags = {
+      Environment = "Production"
+      Owner       = "Anthony Brignano"
+      Project     = "aws-config"
+      Repository  = "https://www.github.com/brignano/aws"
+      Terraform   = "https://app.terraform.io/app/brignano/workspaces/aws-config"
+    }
+  }
 }
