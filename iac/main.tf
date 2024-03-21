7a4aa919389453cba4f6ed70b1458bb37c5eba48
anthony <abrignano@comcast.net>
2024-03-21 01:29:25 +0000

    Revert "update `A` alias redirect record to `CNAME`"
    This reverts commit 1e9f001464bf79c67fcf1f380cfae6f885d69cde.

--- iac/main.tf
+++ iac/main.tf
@@ -73,8 +73,12 @@ resource "aws_route53_record" "anthonybrignano_com" {
 resource "aws_route53_record" "www_anthonybrignano_com" {
   zone_id = aws_route53_zone.anthonybrignano_com.zone_id
   name    = "www.${local.domain_name.backup}"
-  type    = "CNAME"
-  records = [ local.domain_name.backup ]
+  type    = "A"
+  alias {
+    name                   = local.domain_name.backup
+    zone_id                = aws_route53_zone.anthonybrignano_com.zone_id
+    evaluate_target_health = false
+  }
 }
 
 resource "aws_s3_bucket" "redirect" {
