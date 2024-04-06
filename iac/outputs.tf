4367d95ad4310228ebc8f560130c106e1ed22b15
Anthony Brignano <abrignano@comcast.net>
2024-04-06 02:56:53 +0000

    remove unnecessary outputs

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -10,10 +10,6 @@ output "brignano_io_zone_id" {
   value = aws_route53_zone.default.zone_id
 }
 
-output "brignano_io_name_servers" {
-  value = aws_route53_zone.default.name_servers
-}
-
 output "anthony_brignano_com_name" {
   value = aws_route53_zone.backup.name
 }
@@ -21,8 +17,3 @@ output "anthony_brignano_com_name" {
 output "anthony_brignano_com_zone_id" {
   value = aws_route53_zone.backup.zone_id
 }
-
-output "anthony_brignano_com_name_servers" {
-  value = aws_route53_zone.backup.name_servers
-}
-
