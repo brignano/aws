28e469002c8e42b6c3e4eda6d2c79f8bb30a5f20
Anthony Brignano <abrignano@comcast.net>
2024-04-06 03:10:07 +0000

    add more outputs

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,19 +1,34 @@
-output "lambda_forward_email_arn" {
-  value = aws_lambda_function.email.arn
+output "aws_region" {
+  value       = data.aws_region.current.name
+  description = "The current AWS region"
 }
 
-output "brignano_io_name" {
-  value = aws_route53_zone.default.name
+output "aws_account_id" {
+  value       = data.aws_caller_identity.current.account_id
+  description = "The current AWS account ID"
 }
 
-output "brignano_io_zone_id" {
-  value = aws_route53_zone.default.zone_id
+output "email_forwarding_lambda_arn" {
+  value       = aws_lambda_function.email.arn
+  description = "The ARN of the Lambda function that is responsible for forwarding emails."
 }
 
-output "anthony_brignano_com_name" {
-  value = aws_route53_zone.backup.name
+output "primary_website" {
+  value       = aws_route53_zone.default.name
+  description = "The default website domain name."
 }
 
-output "anthony_brignano_com_zone_id" {
-  value = aws_route53_zone.backup.zone_id
+output "primary_hosted_zone_id" {
+  value       = aws_route53_zone.default.zone_id
+  description = "The default hosted zone ID."
+}
+
+output "secondary_website" {
+  value       = aws_route53_zone.backup.name
+  description = "The backup website domain name."
+}
+
+output "backup_hosted_zone_id" {
+  value       = aws_route53_zone.backup.zone_id
+  description = "The backup hosted zone ID."
 }
