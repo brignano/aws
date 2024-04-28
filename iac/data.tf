42cb78f1ca1c99f118f07dfe69b3f50cf899810d
anthony <abrignano@comcast.net>
2024-04-28 06:23:02 +0000

    refactor iam policies

--- iac/data.tf
+++ iac/data.tf
@@ -5,3 +5,71 @@ data "archive_file" "email" {
   source_file = "lambda/forward_email.py"
   output_path = "lambda/forward_email.zip"
 }
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "lambda_logs" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${aws_lambda_function.email.function_name}:*",]
+  }
+}
+
+data "aws_iam_policy_document" "s3_get_object" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = ["${aws_s3_bucket.email.arn}/emails/*",]
+  }
+}
+
+data "aws_iam_policy_document" "send_raw_email" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ses:SendRawEmail",
+    ]
+
+    resources = [
+      "arn:aws:ses:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:identity/*",
+      ]
+  }
+}
+
+data "aws_iam_policy_document" "s3_bucket" {
+  statement {
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.email.arn}/emails/*"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:referer"
+      values = ["data.aws_caller_identity.current.account_id"]
+    }
+  }
+}
--- iac/main.tf
+++ iac/main.tf
@@ -128,36 +128,12 @@ resource "aws_s3_bucket_acl" "email" {
 
 resource "aws_s3_bucket_policy" "email" {
   bucket = aws_s3_bucket.email.id
-  policy = {
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.email.arn}/emails/*"
-        Condition = {
-          StringEquals = {
-            "aws:Referer" = data.aws_caller_identity.current.account_id
-          }
-        }
-      }
-    ]
-  }
+  policy = aws_iam_policy.s3_bucket.json 
 }
 
 resource "aws_iam_role" "email" {
   name               = "LambdaSesForwarderRole"
-  assume_role_policy = {
-    statement = {
-      actions = ["sts:AssumeRole"]
-      effect  = "Allow"
-      principals = {
-        type        = "Service"
-        identifiers = ["lambda.amazonaws.com"]
-      }
-    }
-  }
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
 resource "aws_cloudwatch_log_group" "email_logs" {
@@ -165,18 +141,6 @@ resource "aws_cloudwatch_log_group" "email_logs" {
   retention_in_days = 30
 }
 
-data "aws_iam_policy_document" "lambda_logs" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-    ]
-
-    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${aws_lambda_function.email.function_name}:*",]
-  }
-}
 
 resource "aws_iam_policy" "lambda_logs" {
   name        = "LambdaLogsPolicy"
@@ -189,18 +153,6 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = aws_iam_policy.lambda_logs.arn
 }
 
-data "aws_iam_policy_document" "s3_get_object" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "s3:GetObject",
-    ]
-
-    resources = ["${aws_s3_bucket.email.arn}/emails/*",]
-  }
-}
-
 resource "aws_iam_policy" "s3_get_object" {
   name        = "S3GetObjectPolicy"
   description = "Allow Lambda to get emails from S3."
@@ -210,20 +162,6 @@ resource "aws_iam_policy" "s3_get_object" {
 resource "aws_iam_role_policy_attachment" "s3_get_object" {
   role       = aws_iam_role.email.name
   policy_arn = aws_iam_policy.s3_get_object.arn
-}
-
-data "aws_iam_policy_document" "send_raw_email" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "ses:SendRawEmail",
-    ]
-
-    resources = [
-      "arn:aws:ses:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:identity/*",
-      ]
-  }
 }
 
 resource "aws_iam_policy" "send_raw_email" {
