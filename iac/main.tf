7812a9851e842e5125856dd5efddcad7bdc8c1c9
anthony <abrignano@comcast.net>
2024-04-28 07:52:07 +0000

    update `s3:GetObject` to `s3:PutObject`

--- iac/main.tf
+++ iac/main.tf
@@ -134,7 +134,7 @@ resource "aws_s3_bucket_policy" "email" {
 data "aws_iam_policy_document" "s3_bucket" {
   statement {
     actions = [
-      "s3:GetObject",
+      "s3:PutObject",
     ]
 
     resources = [
